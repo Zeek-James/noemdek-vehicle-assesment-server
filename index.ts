4b41625326cb311d3096eeb611060e9af02e4cca
Isaac James <chizuruokej@yahoo.com>
2023-07-09 23:12:17 +0100

    fixs cors error

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import { vehicleData } from "./constants/data";
-const app = express();
 
+const app = express();
 const port = 3440;
 
 // Enable CORS for all routes
 app.use(cors());
 
-app.get("/vehicles", (req: Request, res: Response) => {
+app.get("/vehicles", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "GET"); // Allow GET requests
   res.json(vehicleData);
 });
 
